252c8b42e4e4ab42f25ee48568cef79e72c638a2
jonatan.thunell@yahoo.com <jonatan.thunell@yahoo.com>
2025-02-27 17:35:41 +0100

    Product page start

---
+++ WebshopShared/Models/Boardgame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopShared.Models
+{
+    public class Boardgame : IProduct
+    {
+        public string ArtNr { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public int MinAge { get; set; }
+        public int PlayTime { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public string ImageText { get; set; }
+        public int Stock { get; set; }
+    }
+}
