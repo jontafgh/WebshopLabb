81de4d51d0ab83e00fe112e07e3d97ca3b6682f2
jonatan.thunell@yahoo.com <jonatan.thunell@yahoo.com>
2025-03-03 22:14:47 +0100

    Form styling, Table styling, ProductPreview Styling

--- WebshopShared/Person.cs
+++ WebshopShared/Person.cs
@@ -10,11 +10,18 @@ namespace WebshopShared
     public class Person
     {
         [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string FirstName { get; set; }
+
         [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string LastName { get; set; }
+
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Phone]
         public string PhoneNumber { get; set; }
 
         [ValidateComplexType]
@@ -24,12 +31,19 @@ namespace WebshopShared
     public class Address
     {
         [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Street { get; set; }
+
         [Required]
+        [StringLength(20, MinimumLength = 1)]
         public string PostalCode { get; set; }
+
         [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string City { get; set; }
+
         [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Country { get; set; }
     }
 }
