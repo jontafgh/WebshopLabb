a07c8237fd12df61c80b8b43ddc620777e619110
jonatan.thunell@yahoo.com <jonatan.thunell@yahoo.com>
2025-03-04 15:27:57 +0100

    Before Main Merge

---
+++ WebshopFrontend/Services/Interfaces/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using WebshopShared;
+
+namespace WebshopFrontend.Services.Interfaces
+{
+    public interface ICartService
+    {
+        Task<CartItemDto> AddItem(int productId);
+        Task<CartItemDto> UpdateItem(int itemId, int quantity);
+        Task<CartItemDto> RemoveItem(int itemId);
+        Task<CartItemDto> GetItem(int itemId);
+        Task<CartItemDto[]> GetAll();
+        Task ClearCart();
+    }
+}
---
+++ WebshopFrontend/Services/LocalStorageCartService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.JSInterop;
+using WebshopFrontend.Services.Interfaces;
+using WebshopShared;
+
+namespace WebshopFrontend.Services
+{
+    public class LocalStorageCartService(IJSRuntime js) : ICartService
+    {
+        private readonly IJSRuntime _js = js;
+
+        public async Task<CartItemDto> AddItem(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItemDto> UpdateItem(int itemId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItemDto> RemoveItem(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItemDto> GetItem(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItemDto[]> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ClearCart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
